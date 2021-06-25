a4d66dc9571c14cb8ccbc2b9abd6095b9cd89164
TefyRabih <tefyrabih@gmail.com>
2021-06-25 08:31:37 -0500

    imagen retorno y funcion onclick

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,11 @@
 const validator = {
   // ...
 };
+//Funcion del boton comprar
+function button{
+document.getElementById('Bienvenida').style.display = 'none';
+document.getElementById('card').removeAttribute ('hidden');
+document.getElementById('card').style.display = 'block';
+}
 
 export default validator;
