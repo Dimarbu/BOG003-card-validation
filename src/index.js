f835d8600821255c2ddbb453a9350cc7722615a5
TefyRabih <tefyrabih@gmail.com>
2021-06-25 18:59:52 -0500

    Funcionalidad botones regresar y validar

--- src/index.js
+++ src/index.js
@@ -12,15 +12,33 @@ import validator from './validator.js';
         document.getElementById('Bienvenida').style.display = 'none';
         document.getElementById('card').removeAttribute ('hidden');
         document.getElementById('card').style.display = 'block';
-    })
+    });
 
-    //Funcion del boton comprar cámara
+//Funcion del boton comprar cámara
     document.getElementById("bttn2").addEventListener("click",()=>{
         document.getElementById("Bienvenida").style.display = "none";
         document.getElementById("card").removeAttribute ("hidden");
         document.getElementById("card").style.display = "block";
-    })
+    });
 
+//Funcion del boton regresar segunda pantalla
+    document.getElementById("behind").addEventListener("click",()=>{
+        document.getElementById("card").style.display = "none";
+        document.getElementById("Bienvenida").style.display = "block";
+    });
+
+//Funcion del boton validar pago
+    document.getElementById("validateOk").addEventListener("click",()=>{
+       document.getElementById("card").style.display = "none";
+       document.getElementById("payOk").removeAttribute ("hidden");
+       document.getElementById("payOk").style.display = "block";
+    });
+
+//Funcion del boton regresar tercera pantalla
+document.getElementById("behind2").addEventListener("click",()=>{
+    document.getElementById("payOk").style.display = "none";
+    document.getElementById("Bienvenida").style.display = "block";
+});
 
 
 console.log(validator);
