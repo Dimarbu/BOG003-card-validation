140ce6afbee162f56c38d5181ce5c1a550b44722
Diana Buitrago <dianabuitrago54@hotmail.es>
2021-06-25 18:16:34 -0500

    Funcionalidad de los botones primer pantalla

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import validator from './validator.js';
 
+//Funcion del boton comprar
+/*function button{
+    document.getElementById('Bienvenida').style.display = 'none';
+    document.getElementById('card').removeAttribute ('hidden');
+    document.getElementById('card').style.display = 'block';
+    }*/
+
+//Funcion del boton comprar audifonos
+    document.getElementById("bttn").addEventListener("click",()=>{
+        document.getElementById('Bienvenida').style.display = 'none';
+        document.getElementById('card').removeAttribute ('hidden');
+        document.getElementById('card').style.display = 'block';
+    })
+
+    //Funcion del boton comprar cámara
+    document.getElementById("bttn2").addEventListener("click",()=>{
+        document.getElementById("Bienvenida").style.display = "none";
+        document.getElementById("card").removeAttribute ("hidden");
+        document.getElementById("card").style.display = "block";
+    })
+
+
+
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,11 +1,6 @@
 const validator = {
   // ...
 };
-//Funcion del boton comprar
-function button{
-document.getElementById('Bienvenida').style.display = 'none';
-document.getElementById('card').removeAttribute ('hidden');
-document.getElementById('card').style.display = 'block';
-}
+
 
 export default validator;
