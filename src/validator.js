e472ad246867bf918abcd980fd0b710e641b12e9
Diana Buitrago <dianabuitrago54@hotmail.es>
2021-06-26 16:52:38 -0500

    se anexan las funciones del boton

--- src/validator.js
+++ src/validator.js
@@ -2,5 +2,10 @@ const validator = {
   // ...
 };
 
+const compras = function () {
+  document.getElementById('Bienvenida').style.display = 'none';
+  document.getElementById('card').removeAttribute ('hidden');
+  document.getElementById('card').style.display = 'block';
+}
 
-export default validator;
+export default {validator, compras};
