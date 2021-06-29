105516da0034b08cf5e99f671a904951f8748c18
Diana Buitrago <dianabuitrago54@hotmail.es>
2021-06-29 10:27:13 -0500

    se crea la variable y función que guarda el número de tarjeta

--- src/index.js
+++ src/index.js
@@ -8,36 +8,42 @@ import validator from './validator.js';
     }*/
 
 //Funcion del boton comprar audifonos
-    document.getElementById("bttn").addEventListener("click",()=>{
-        document.getElementById('Bienvenida').style.display = 'none';
-        document.getElementById('card').removeAttribute ('hidden');
-        document.getElementById('card').style.display = 'block';
-    });
+document.getElementById("bttn").addEventListener("click", () => {
+    document.getElementById('Bienvenida').style.display = 'none';
+    document.getElementById('card').removeAttribute('hidden');
+    document.getElementById('card').style.display = 'block';
+});
 
 //Funcion del boton comprar cámara
-    document.getElementById("bttn2").addEventListener("click",()=>{
-        document.getElementById('Bienvenida').style.display = 'none';
-        document.getElementById('card').removeAttribute ('hidden');
-        document.getElementById('card').style.display = 'block';
-    });
+document.getElementById("bttn2").addEventListener("click", () => {
+    document.getElementById('Bienvenida').style.display = 'none';
+    document.getElementById('card').removeAttribute('hidden');
+    document.getElementById('card').style.display = 'block';
+});
 
-//  Funcion del boton regresar segunda pantalla
-    document.getElementById("behind").addEventListener("click",()=>{
+//  Funcion del boton regresar de la segunda pantalla a la primera
+document.getElementById("behind").addEventListener("click", () => {
+    document.getElementById('card').style.display = 'none';
+    document.getElementById('Bienvenida').style.display = 'block';
+});
+
+//Funcion del boton validar pago en la segunda pantalla
+document.getElementById("validateOk").addEventListener("click", () => {
+    let numCard = document.getElementById("numCard").value;
+    //console.log("numCard="+numCard);
+    if (validator.validator.isvalid(numCard)) {
         document.getElementById('card').style.display = 'none';
-        document.getElementById('Bienvenida').style.display = 'block';
-    });    
-
-//Funcion del boton validar pago
-    document.getElementById("validateOk").addEventListener("click",()=>{
-        document.getElementById('card').style.display ='none';
-        document.getElementById('payOk').removeAttribute ('hidden');
-        document.getElementById('payOk').style.display ='block';
-    });   
+        document.getElementById('payOk').removeAttribute('hidden');
+        document.getElementById('payOk').style.display = 'block';
+    } else {
+        alert("Tarjeta invalida");
+    }
+});
 
 //Funcion del boton regresar tercera pantalla
-    document.getElementById("payOk").addEventListener("click",()=>{
-       document.getElementById('payOk').style.display = 'none';
-  document.getElementById('Bienvenida').style.display = 'block';
-    });
+document.getElementById("payOk").addEventListener("click", () => {
+    document.getElementById('payOk').style.display = 'none';
+    document.getElementById('Bienvenida').style.display = 'block';
+});
 
-console.log (validator);
+console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,28 +1,7 @@
 const validator = {
   // ...
   isvalid: (creditCardNumber) => {
-    let sum = 0;
-    let multiply = 0;
-    for (let i = 0;i < creditCardNumber.lenght;i++){
-      if (i % 2 === 0) {
-        //....
-        multiply = (creditCardNumber[i] - '0');
-      }
-      else {
-        //Multiplicar if *2
-        multiply = (creditCardNumber[i] - '0') * 2;
-        if (multiply >= 10) {
-          multiply = (multiply - 10) + 1;
-        }
-      }
-      sum += multiply;
-    }
-    if (sum % 10 === 0) {
-      return true;
-    }
-    else {
-      return false;
-    }
+    
   },
 
 };
