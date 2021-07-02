c2ce844ffb7e900258c39a575ce577fdeb7f0af1
TefyRabih <tefyrabih@gmail.com>
2021-07-02 14:29:33 -0500

    Funcion metodo maskify

--- src/index.js
+++ src/index.js
@@ -9,49 +9,58 @@ import validator from './validator.js';
 
 //Funcion del boton comprar audifonos
 document.getElementById("bttn").addEventListener("click", () => {
-    document.getElementById('Bienvenida').style.display = 'none';
-    document.getElementById('Bienvenida2').style.display = 'none';
-    document.getElementById('card').removeAttribute('hidden');
-    document.getElementById('card').style.display = 'block';
+  document.getElementById('Bienvenida').style.display = 'none';
+  document.getElementById('Bienvenida2').style.display = 'none';
+  document.getElementById('card').removeAttribute('hidden');
+  document.getElementById('card').style.display = 'block';
 });
 
 //Funcion del boton comprar cámara
 document.getElementById("bttn2").addEventListener("click", () => {
-    document.getElementById('Bienvenida').style.display = 'none';
-    document.getElementById('Bienvenida2').style.display = 'none';
-    document.getElementById('card').removeAttribute('hidden');
-    document.getElementById('card').style.display = 'block';
+  document.getElementById('Bienvenida').style.display = 'none';
+  document.getElementById('Bienvenida2').style.display = 'none';
+  document.getElementById('card').removeAttribute('hidden');
+  document.getElementById('card').style.display = 'block';
 });
 
 //  Funcion del boton regresar de la segunda pantalla a la primera
 document.getElementById("behind").addEventListener("click", () => {
-    document.getElementById('card').style.display = 'none';
-    document.getElementById('Bienvenida').style.display = 'block';
-    document.getElementById('Bienvenida2').style.display = 'block';
+  document.getElementById('card').style.display = 'none';
+  document.getElementById('Bienvenida').style.display = 'block';
+  document.getElementById('Bienvenida2').style.display = 'block';
 });
 
 //Funcion del boton validar pago en la segunda pantalla
 document.getElementById("validateOk").addEventListener("click", () => {
-    let creditCardNumber = document.getElementById("numCard").value;
-   //const validCard = validator.validator.isValid(creditCardNumber);
-    if (validator.isValid(creditCardNumber)) {
-        document.getElementById('card').style.display = 'none';
-        document.getElementById('payOk').removeAttribute('hidden');
-        document.getElementById('payOk').style.display = 'block';
-    } else {
-        alert("Tarjeta invalida");
-   
-    }
-    let getValue = document.getElementById("numCard").value;
-    document.getElementById("dataCard").innerHTML = getValue;
-
+  let creditCardNumber = document.getElementById("numCard").value;
+  //const validCard = validator.validator.isValid(creditCardNumber);
+  const validacionTarjeta = tarjetaValida(creditCardNumber);
+  if (validacionTarjeta) {
+    tarjetaMaskify(creditCardNumber);
+  }
 });
 
+function tarjetaMaskify(creditCardNumber) {
+  document.getElementById("dataCard").innerHTML = validator.maskify(creditCardNumber);
+}
+
+function tarjetaValida(creditCardNumber) {
+  let valid = validator.isValid(creditCardNumber);
+  if (valid === true) {
+    document.getElementById('card').style.display = 'none';
+    document.getElementById('payOk').removeAttribute('hidden');
+    document.getElementById('payOk').style.display = 'block';
+    return true;
+  } else {
+    alert("Tarjeta invalida");
+  }
+
+}
 //Funcion del boton regresar tercera pantalla
 document.getElementById("behind2").addEventListener("click", () => {
-    document.getElementById('payOk').style.display = 'none';
-    document.getElementById('Bienvenida').style.display = 'block';
-    document.getElementById('Bienvenida2').style.display = 'block';
+  document.getElementById('payOk').style.display = 'none';
+  document.getElementById('Bienvenida').style.display = 'block';
+  document.getElementById('Bienvenida2').style.display = 'block';
 });
 
 //console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,31 +1,48 @@
 const validator = {
   // Sera la encargada de verificar que el numero de tarjeta sea valido segun el algoritmo de luhn
-    isValid: (creditCardNumber) => {
-      let reversed = (creditCardNumber.split("").reverse())
-      let numPar = 0;
-      let numImpar = 0;
-      let sum = 0;
-      for (let i=0; i<reversed.length; i++) {
-        //console.log(creditCardNumber[i]);
-        if ((i % 2) == 1) {
-          numPar = parseInt(reversed[i]) * 2;
-          if (numPar >= 10) {
-            numPar = (numPar - 10) + 1;
-          }
-          sum += numPar;
-        } else {
-          numImpar = parseInt(reversed[i]);
-          sum += numImpar;
+  isValid: (creditCardNumber) => {
+    let reversed = (creditCardNumber.split("").reverse())
+    let numPar = 0;
+    let numImpar = 0;
+    let sum = 0;
+    for (let i = 0; i < reversed.length; i++) {
+      //console.log(creditCardNumber[i]);
+      if ((i % 2) == 1) {
+        numPar = parseInt(reversed[i]) * 2;
+        if (numPar >= 10) {
+          numPar = (numPar - 10) + 1;
         }
+        sum += numPar;
+      } else {
+        numImpar = parseInt(reversed[i]);
+        sum += numImpar;
       }
-      //console.log("numero es"+ sum);
-      if (sum % 10 == 0) {
-        return true;
-      }
-      else {
-        return false;
-      }
-      //console.log();
     }
+    //console.log("numero es"+ sum);
+    if (sum % 10 == 0) {
+      return true;
+    } else {
+      return false;
+    }
+    //console.log();
+  },
+  maskify: (creditCardNumber) => {
+   let selectNumber = creditCardNumber.length;
+   let maskingNumber = "";
+   for(let i = 0; i<selectNumber-4; i++) {
+  maskingNumber += "*";
+    }
+    return maskingNumber + creditCardNumber.substring(selectNumber-4,selectNumber) 
+
   }
-  export default validator;
+ /* maskify: (creditCardNumber) => {
+    if (creditCardNumber.length <= 6) return creditCardNumber;
+    const lastFour = creditCardNumber.substr(-4);
+    const maskingNumber = creditCardNumber.substr(1, creditCardNumber.length - 5).replace(/\d/g, '*');
+    return `${maskingNumber}${lastFour}`;
+  }*/
+}
+
+
+
+export default validator;
