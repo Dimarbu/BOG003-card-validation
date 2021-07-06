5dd829bf229f3e853d8ffe40721074421f3a02e5
TefyRabih <tefyrabih@gmail.com>
2021-07-06 12:46:34 -0500

    Comentarios y validacion campos vacios

--- src/index.js
+++ src/index.js
@@ -29,22 +29,36 @@ document.getElementById("behind").addEventListener("click", () => {
   document.getElementById('Bienvenida').style.display = 'block';
   document.getElementById('Bienvenida2').style.display = 'block';
 });
-
 //Funcion del boton validar pago en la segunda pantalla
 document.getElementById("validateOk").addEventListener("click", () => {
-  let creditCardNumber = document.getElementById("numCard").value;
-  document.getElementById("voucher").innerHTML = Math.floor(Math.random() * 100);
-  //const validCard = validator.validator.isValid(creditCardNumber);
-  const validacionTarjeta = tarjetaValida(creditCardNumber);
-  if (validacionTarjeta) {
-    tarjetaMaskify(creditCardNumber);
+  //Valida que no existan los campos vacios
+  if(validaCampos()){
+    alert("Por favor revisar que no existan campos vacíos");
+  }
+  //Al no encontrar campos vacíos realiza las funciones de validar tarjeta y enmascarar 
+  else{
+    let creditCardNumber = document.getElementById("numCard").value;
+  //genera un consecutivo aleatorio como comprobante de pago  
+    document.getElementById("voucher").innerHTML = Math.floor(Math.random() * 100);
+    const validacionTarjeta = tarjetaValida(creditCardNumber);
+  //llama la funcion de enmascarar tarjeta
+    if (validacionTarjeta) {
+      tarjetaMaskify(creditCardNumber);
+    }
   }
 });
-
-function tarjetaMaskify(creditCardNumber) {
-  document.getElementById("dataCard").innerHTML = validator.maskify(creditCardNumber);
+//Funcion de validar campos vacios
+function validaCampos(){
+  let dataName = document.getElementById("name").value;
+  let creditCardNumber = document.getElementById("numCard").value;
+ if(dataName.length == 0 || creditCardNumber.length == 0){
+   return true; 
+ }
+   else {
+   return false;
+   }
 }
-
+// Funcion para validar el numero de la tarjeta y enviar a la tercera pagina
 function tarjetaValida(creditCardNumber) {
   let valid = validator.isValid(creditCardNumber);
   if (valid === true) {
@@ -55,7 +69,10 @@ function tarjetaValida(creditCardNumber) {
   } else {
     alert("Tarjeta invalida");
   }
-
+}
+//funcion de enmascarar numero de tarjeta 
+function tarjetaMaskify(creditCardNumber) {
+  document.getElementById("dataCard").innerHTML = validator.maskify(creditCardNumber);
 }
 //Funcion del boton regresar tercera pantalla
 document.getElementById("behind2").addEventListener("click", () => {
